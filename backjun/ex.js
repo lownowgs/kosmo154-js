c05d890ff3ce85616659fcceafc9c7801e2e93a3
lownowgs <hhsshh1047@gmail.com>
2025-03-26 18:13:06 +0900

    new

--- backjun/ex.js
+++ backjun/ex.js
@@ -87,13 +87,4 @@ console.log((temp===a) ? "Yes": "No");
 
 
 
-// var a=parseInt(input[0]);
-// var b=parseInt(input[1]);
-
-// console.log(a+b);
-// console.log(a-b);
-// console.log(a*b);
-
-// console.log(parseInt(a/b));
-// console.log(a%b);
 
